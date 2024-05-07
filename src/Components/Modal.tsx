5a8ec7022d5dcb1d56143dc68b531bf9b690d6b6
Rikuto <orikutobasuke@gmail.com>
2024-05-08 02:54:35 +0900

    Modal完成

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,9 +1,21 @@
 'use client'
 
-export function Modal() {
+import { useState, useEffect } from 'react'
+import style from '@/styles/Components/modal.module.scss'
+
+type Props = {
+    children: React.ReactNode
+}
+
+export function Modal(props: Props) {
+
     return (
         <>
-            <div></div>
+            <div className={style.modalWrap}>
+                <div className={style.modalBox}>
+                    {props.children}
+                </div>
+            </div>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 'use client'
 
 import Link from 'next/link'
-import style from "@/styles/page.module.css";
+import style from "@/styles/page.module.scss";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className={style.pageWrap}>
+        <header className={style.header}>
+          <h1 className={style.title}>2年生 Next.js課題</h1>
+        </header>
+        <div className={style.pageContents}>
+          <Link className={style.link} href={'./Modal'}>Modal</Link>
+          <Link className={style.link} href={'./Timer'}>Timer</Link>
+        </div>
+      </div>
+    </>
   );
 }
