cddd1029806ec79bf5d34ffb59d03073cbd50882
snhrayk <ayaka21sunahara@gmail.com>
2024-05-16 18:27:12 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,7 @@ import style from "@/styles/page.module.scss";
 export default function Home() {
   return (
     <>
-      <div className={style.pageWrap}>
-        <header className={style.header}>
-          <h1 className={style.title}>2年生 Next.js課題</h1>
-        </header>
-        <div className={style.pageContents}>
-          <Link className={style.link} href={'./Modal'}>Modal</Link>
-          <Link className={style.link} href={'./Timer'}>Timer</Link>
-        </div>
-      </div>
+
     </>
   );
 }
