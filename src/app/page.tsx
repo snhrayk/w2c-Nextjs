9ebce8de389c41834237f8e9479b852bf591d7da
snhrayk <ayaka21sunahara@gmail.com>
2024-05-16 18:31:47 +0900

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
+import Link from "next/link";
 import style from "@/styles/page.module.scss";
 
 export default function Home() {
   return (
     <>
-
+      <h1>poko</h1>
     </>
   );
 }
