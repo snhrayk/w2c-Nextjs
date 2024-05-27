b6a07c87a7c11efbe4e0af6e74fb113fd115eb70
snhrayk <ayaka21sunahara@gmail.com>
2024-05-27 18:57:56 +0900

    最初のページ作成中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,15 @@ import style from "@/styles/page.module.scss";
 export default function Home() {
   return (
     <>
-      <h1>poko</h1>
+      <header className={style.header}>
+        <h1>2年生 Next.js課題</h1>
+      </header>
+      <p className={style.modal}>
+        <a href="#">Modal</a>
+      </p>
+      <p className={style.timer}>
+        <a href="">Timer</a>
+      </p>
     </>
   );
 }
